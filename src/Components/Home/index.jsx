c8df7cee8d4b60f350754880ff804a15821654c3
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2023-12-13 22:54:21 +0500

    Initial commit

---
+++ src/Components/Home/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Flex, Image, Menu, Switch } from "antd";
+import logo from "../../assets/images/logo_new.png";
+import { Header } from "antd/es/layout/layout";
+
+
+const Home = () => {
+  const [theme, setTheme] = React.useState("dark");
+  const changeTheme = (value) => {
+    setTheme(value ? "dark" : "light");
+  };
+
+  return (
+    <Header>
+      <div className="container">
+        <Flex justify="space-between" align="center" padding={5}>
+          <div className="site-header__start">
+            <Image src={logo} alt="" width="150px" />
+          </div>
+          <div className="site-header__end">
+            <Switch
+              checked={theme === "dark"}
+              onChange={changeTheme}
+              checkedChildren="Dark"
+              unCheckedChildren="Light"
+            />
+          </div>
+        </Flex>
+      </div>
+    </Header>
+  );
+};
+
+export default Home;
---
+++ src/Root/index.jsx
@@ -0,0 +1,11 @@
+import Home from "../Components/Home";
+
+const Root = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default Root;
